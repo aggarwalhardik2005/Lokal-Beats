819c0dd8db63a4c67e3c4ec9e4b79b67cdef744c
aggarwalhardik2005 <hardikaggarwal01@gmail.com>
2026-01-23 00:31:53 +0530

    final

--- src/helpers/types.ts
+++ src/helpers/types.ts
@@ -1,14 +1,21 @@
-import { Track } from 'react-native-track-player'
+export interface Track {
+  id: string;
+  title: string;
+  artist?: string;
+  artwork?: string;
+  url: string;
+  duration?: number;
+}
 
 export type Playlist = {
-	name: string
-	tracks: Track[]
-	artworkPreview: string
-}
+  name: string;
+  tracks: Track[];
+  artworkPreview: string;
+};
 
 export type Artist = {
-	name: string
-	tracks: Track[]
-}
+  name: string;
+  tracks: Track[];
+};
 
-export type TrackWithPlaylist = Track & { playlist?: string[] }
+export type TrackWithPlaylist = Track & { playlist?: string[] };
